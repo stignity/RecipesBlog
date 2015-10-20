2fb579a370c3100e97a904ae5b34fb05a5d563e8
Anders Isaksen <stignity@gmail.com>
2015-10-20 12:23:17 +0000

    Modified models and added test classes, validation checking

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,3 +1,9 @@
 class Chef < ActiveRecord::Base
-    
+    has_many :recipes
+    before_save { self.email = email.downcase }
+    validates :chefname, presence: true, length: { minimum: 4, maximum: 100 }
+    VALID_EMAIL_REGEX = /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
+    validates :email, presence: true, length: { maximum: 105 },
+                                      uniqueness: { case_sensitive: false },
+                                      format: { with: VALID_EMAIL_REGEX }
 end
